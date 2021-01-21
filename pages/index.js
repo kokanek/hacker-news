6e641a85ce3633d463d48f6a11920af519e2285e
kokanek <kapeel.kokane@gmail.com>
2021-01-21 21:20:50 +0530

    Table working with antd integrated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import 'antd/dist/antd.css'
+import { Statistic, Row, Col, Button, Divider } from 'antd';
+import { UserOutlined, ClockCircleOutlined, LinkOutlined } from '@ant-design/icons';
+
+function TableRow({item}) {
+  console.log('table row props: ', item);
+  return (
+    <Row gutter={16} className={styles.fullWidth}>
+      <Col span={3}>
+        <Statistic title="POINTS" value={item.score} valueStyle={{ color: '#FB651E' }} />
+      </Col>
+      <Col span={3}>
+        <Statistic title="COMMENTS" value={item.descendants} valueStyle={{ color: '#FB651E' }} />
+      </Col>
+      <Col span={14} className={styles.separator}>
+        <h2>{item.title}</h2>
+        <ClockCircleOutlined />{` 15 mins ago `}
+        <UserOutlined /> {item.by}
+        <LinkOutlined /> {` google.com `}
+      </Col>
+      <Col span={1}>
+        <Button type="primary" danger>Open</Button>
+      </Col>
+      <Divider />
+    </Row>
+  )
+}
 
 export default function Home(props) {
   return (
@@ -8,20 +35,30 @@ export default function Home(props) {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        {props.values.map(v => <p>{v.by}</p>)}
+      
+      <div className={styles.fullWidth}>
+        {props.values.map(v => <TableRow item={v} />)}
       </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      {/* <footer className={styles.footer}>
+        <p>
+          Created with ❤️ by 
+           <a
+            href="https://comscience.now.sh"
+            target="_blank"
+          >
+            <span>Kapeel Kokane</span>
+          </a>
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          </a>
+        </p>
+      </footer> */}
     </div>
   )
 }
@@ -29,12 +66,10 @@ export default function Home(props) {
 export async function getServerSideProps(context) {
 
   const { content='latest', page=1 } = context.query;
-  console.log('context in getStaticProps: ', context.query, context.path);
   const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
   let posts = await res.json()
   
   const slicedPosts = posts.slice(Number(page)*10, (Number(page)+1)*10);
-  console.log('sliced: ', slicedPosts);
   const articles = slicedPosts.map(post => fetch(`https://hacker-news.firebaseio.com/v0/item/${post}.json?print=pretty`))
   const data = await Promise.all(articles);
 
